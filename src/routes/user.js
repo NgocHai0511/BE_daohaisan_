ef80bc4c5f36e76231bca5ed15141a0057057ce2
Tien Nhan <nhandzzcuteo@gmail.com>
2023-11-25 16:15:45 +0700

    nhan - getUser and resetPass

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const orderController = require("../controllers/orderController");
-const { upload } = require("../config/setupfirebase");
+const { upload } = require("../config/setUpFirebase");
+const validateToken = require("../config/validateTokenHandler");
 
 //[GET] api/users
 router.get("/users", userController.getAllUser);
 //[GET] api/user/:id
-router.get("/user/:id", userController.getUser);
+router.get("/user/:id", validateToken, userController.getUser);
 //[GET] api/customers
 router.get("/customers", userController.getAllCustomer);
 //[POST] api/user/login
@@ -14,14 +15,34 @@ router.post("/user/login", userController.loginUser);
 //[POST] api/user/
 router.post("/user/register", userController.registerUser);
 //[PUT] api/user
-router.put("/user", upload.single("avatar"), userController.updateUser);
+router.put(
+  "/user",
+  validateToken,
+  upload.single("avatar"),
+  userController.updateUser
+);
 
 //[GET] api/user/cart/:id
-router.get("/user/cart/:id", userController.getCart);
+router.get("/user/cart/:id", validateToken, userController.getCart);
 //[POST] api/user/addCartItem
-router.post("/user/addCartItem", userController.addProductToCart);
+router.post(
+  "/user/addCartItem",
+  validateToken,
+  userController.addProductToCart
+);
 //[PUT] api/user/removeCartItem
-router.put("/user/removeCartItem", userController.removeProductFromCart);
+router.put(
+  "/user/removeCartItem",
+  validateToken,
+  userController.removeProductFromCart
+);
+
+//POST api/resetpass/checkmail
+router.post("/resetpass/checkMail", userController.checkMailAndSendCode);
+//POST api/resetpass/checkmail
+router.post("/resetpass/checkResetCode", userController.checkResetCode);
+//POST api/resetpass/checkmail
+router.put("/resetpass/", userController.changePassword);
 
 //[GET] api/order
 router.get("/orders", orderController.getAllOrder);
