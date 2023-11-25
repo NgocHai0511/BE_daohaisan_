e12977764d2c4cb50a6ec7b8162bbbbb0f522dd6
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-25 17:17:07 +0700

    fix bug import query

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,33 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const productController = require("../controllers/productController");
-const { upload } = require("../config/setupfirebasebase");
-const validateToken = require("../config/validateTokenHandler");
+const productController = require('../controllers/productController');
+const { upload } = require('../config/setupfirebase');
+const validateToken = require('../config/validateTokenHandler');
 
 //GET --- api/products
-router.get("/products", productController.getAllProducts);
+router.get('/products', productController.getAllProducts);
 //GET -- api/products/:searchstring
-router.get("/products/:searchstring", productController.searchProducts);
+router.get('/products/:searchstring', productController.searchProducts);
 //GET --- api/product/:id
-router.get("/product/:id", productController.getSingleProduct);
+router.get('/product/:id', productController.getSingleProduct);
 //GET --- api/product/page
-router.get("/product/", productController.getPanigationProduct);
+router.get('/product/', productController.getPanigationProduct);
 //POST -- api/product
-router.post(
-  "/product",
-  validateToken,
-  upload.single("image"),
-  productController.createProduct
-);
+router.post('/product', validateToken, upload.single('image'), productController.createProduct);
 //PUT --api/product
-router.put(
-  "/product/",
-  validateToken,
-  upload.single("image"),
-  productController.updateProduct
-);
+router.put('/product/', validateToken, upload.single('image'), productController.updateProduct);
 //POST --api/product
-router.post("/product", validateToken, productController.createProduct);
+router.post('/product', validateToken, productController.createProduct);
 //DELETE --api/product
-router.delete("/product/:id", validateToken, productController.deleteProduct);
+router.delete('/product/:id', validateToken, productController.deleteProduct);
 module.exports = router;
